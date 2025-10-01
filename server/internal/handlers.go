a8403a53a7c52fa6f9cf95c61903feed59a2c830
Ameya Rao <ameyarao98@gmail.com>
2025-10-01 13:14:48 +0100

    add yaak for request testing

--- server/internal/handlers.go
+++ server/internal/handlers.go
@@ -17,7 +17,7 @@ func NewHandler() Handler {
 }
 
 func (h Handler) Health(w http.ResponseWriter, r *http.Request) {
-	if _, err := w.Write([]byte("healthy")); err != nil {
+	if _, err := w.Write([]byte("fizz buzz")); err != nil {
 		http.Error(w, "Error writing response", http.StatusInternalServerError)
 	}
 }
